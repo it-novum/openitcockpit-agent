18599c01386846e9d1509f8a87b2e580ca42844a
binsky08 <timo08@triebensky.de>
2019-10-17 09:51:45 +0200

    update drone.yml

---
+++ packages/scripts/build_linux_drone.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+cd ../
+mkdir -p package/usr/bin
+mkdir -p package/etc/openitcockpit-agent/
+
+cp openitcockpit-agent/executables/openitcockpit-agent-python3.linux.bin package/usr/bin/
+cp openitcockpit-agent/exaple_config.cnf package/etc/openitcockpit-agent/config.cnf
+cp openitcockpit-agent/example_customchecks.cnf package/etc/openitcockpit-agent/customchecks.cnf
+cp -r openitcockpit-agent/packages/init package/etc/openitcockpit-agent/
+
+# Debian / Ubuntu x64
+fpm -s dir -t deb -C package --name openitcockpit-agent --vendor "it-novum GmbH" --license "Apache License Version 2.0" --config-files etc/openitcockpit-agent --architecture native --maintainer "<daniel.ziegler@it-novum.com>" --description "openITCOCKPIT Monitoring Agent and remote plugin executor." --url "https://openitcockpit.io" --before-install openitcockpit-agent/packages/preinst.sh --after-install openitcockpit-agent/packages/postinst.sh --before-remove openitcockpit-agent/packages/prerm.sh --version "1.0.0"
+
+# RedHat / CentOS x64
+fpm -s dir -t rpm -C package --name openitcockpit-agent --vendor "it-novum GmbH" --license "Apache License Version 2.0" --config-files etc/openitcockpit-agent --architecture native --maintainer "<daniel.ziegler@it-novum.com>" --description "openITCOCKPIT Monitoring Agent and remote plugin executor." --url "https://openitcockpit.io" --before-install openitcockpit-agent/packages/preinst.sh --after-install openitcockpit-agent/packages/postinst.sh --before-remove openitcockpit-agent/packages/prerm.sh --version "1.0.0"
+
