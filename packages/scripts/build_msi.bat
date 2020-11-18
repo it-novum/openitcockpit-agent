5d2322ee15654ce18748ab97b8fe4fd197d20bef
nook24 <info@nook24.eu>
2020-11-18 14:20:57 +0100

    Remove WixUtilExtension

--- packages/scripts/build_msi.bat
+++ packages/scripts/build_msi.bat
@@ -1,5 +1,5 @@
 cd openitcockpit-agent
 set /p version=<version
 cd msi
-"C:/Program Files (x86)/WiX Toolset v3.11/bin/candle.exe" -v -arch x64 Product.wxs WixUi_Main.wxs LicenseAgreementDlg_HK.wxs -dVersionNumber="%version%" -ext WixNetFxExtension -ext WixFirewallExtension -ext WixUtilExtension
-"C:/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" Product.wixobj WixUi_Main.wixobj LicenseAgreementDlg_HK.wixobj -o openitcockpit-agent.msi -loc Product_en-us.wxl -ext WixUIExtension -ext WixNetFxExtension -ext WixFirewallExtension -ext WixUtilExtension -sw1076
+"C:/Program Files (x86)/WiX Toolset v3.11/bin/candle.exe" -v -arch x64 Product.wxs WixUi_Main.wxs LicenseAgreementDlg_HK.wxs -dVersionNumber="%version%" -ext WixNetFxExtension -ext WixFirewallExtension
+"C:/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" Product.wixobj WixUi_Main.wixobj LicenseAgreementDlg_HK.wixobj -o openitcockpit-agent.msi -loc Product_en-us.wxl -ext WixUIExtension -ext WixNetFxExtension -ext WixFirewallExtension -sw1076
