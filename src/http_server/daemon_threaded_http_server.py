9a46dfe9caebc6549fa8871561ee0c4657bd16a8
nook24 <info@nook24.eu>
2020-11-26 19:07:31 +0100

    Fix Connection refused issue? #43

--- src/http_server/daemon_threaded_http_server.py
+++ src/http_server/daemon_threaded_http_server.py
@@ -5,3 +5,8 @@ from http.server import HTTPServer
 class DaemonThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
     daemon_threads = True
+
+    def finish_request(self, request, client_address):
+        request.settimeout(15)
+        # "super" can not be used because BaseServer is not created from object
+        HTTPServer.finish_request(self, request, client_address)
