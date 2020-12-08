231e16007d3249702b143dfda918ebcd4d02e920
nook24 <info@nook24.eu>
2020-12-08 15:10:22 +0100

    Update Advanced Installer path for version 17.7

--- packages/scripts/build_msi.bat
+++ packages/scripts/build_msi.bat
@@ -1,4 +1,4 @@
 cd openitcockpit-agent-2.0
 set /p version=<version
-"C:\Program Files (x86)\Caphyon\Advanced Installer 17.6\bin\x86\advinst.exe" /edit "C:\Users\kress\openitcockpit-agent-2.0\msi\openitcockpit-agent.aip" \SetVersion "%version%"
-"C:\Program Files (x86)\Caphyon\Advanced Installer 17.6\bin\x86\advinst.exe" /build "C:\Users\kress\openitcockpit-agent-2.0\msi\openitcockpit-agent.aip"
+"C:\Program Files (x86)\Caphyon\Advanced Installer 17.7\bin\x86\advinst.exe" /edit "C:\Users\kress\openitcockpit-agent-2.0\msi\openitcockpit-agent.aip" \SetVersion "%version%"
+"C:\Program Files (x86)\Caphyon\Advanced Installer 17.7\bin\x86\advinst.exe" /build "C:\Users\kress\openitcockpit-agent-2.0\msi\openitcockpit-agent.aip"
